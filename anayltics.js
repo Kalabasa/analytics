b4c70f39ce13eb4ca98d2778f6c78264281bcd81
Lean Rada <3705081+Kalabasa@users.noreply.github.com>
2023-03-03 15:37:13 +0800

    Delete anayltics.js

--- anayltics.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  var script = document.createElement('script');
-  script.dataset.goatcounter = 'https://kalabasa.goatcounter.com/count';
-  script.async = true;
-  script.setAttribute('src', '//gc.zgo.at/count.js');
-  document.head.appendChild(script);
-})();
