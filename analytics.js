9e227105d81ce5507cbb7bd369a64aa0858f7a8c
Lean Rada <3705081+Kalabasa@users.noreply.github.com>
2023-03-03 15:37:00 +0800

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,7 @@
+(function() {
+  var script = document.createElement('script');
+  script.dataset.goatcounter = 'https://kalabasa.goatcounter.com/count';
+  script.async = true;
+  script.setAttribute('src', '//gc.zgo.at/count.js');
+  document.head.appendChild(script);
+})();
