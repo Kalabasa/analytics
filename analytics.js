b722551d287f04c45e370e2d3e9f9461e1deda7a
Kalabasa <Kalabasa@users.noreply.github.com>
2024-03-17 00:20:19 +1100

    Fix prefix

--- analytics.js
+++ analytics.js
@@ -1,18 +1,8 @@
 (function () {
   if (document.currentScript && document.currentScript.dataset.prefix) {
-    if (location.hash.includes('debugAnalytics')) {
-      console.log(document.currentScript);
-      console.log(document.currentScript.dataset);
-      console.log(document.currentScript.dataset.prefix);
-      alert(document.currentScript.dataset.prefix);
-    }
+    const prefix = document.currentScript.dataset.prefix;
     window.goatcounter = window.goatcounter || {};
-    window.goatcounter.path = function (p) {
-      if (location.hash.includes('debugAnalytics')) {
-        console.log(document.currentScript.dataset.prefix + p);
-      }
-      return document.currentScript.dataset.prefix + p;
-    };
+    window.goatcounter.path = function (p) { return prefix + p; };
   }
   var script = document.createElement('script');
   script.dataset.goatcounter = 'https://kalabasa.goatcounter.com/count';
